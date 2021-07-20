82d91993b16e2d54faa763cf04d2a8397f69d8f3
9852worlem <9852worlem@doub.co.kr>
2021-07-20 14:14:48 +0900

    websocket test_01

--- src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
+++ src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
@@ -35,6 +35,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .setRelayPort(61613)
                 .setClientLogin("guest")
                 .setClientPasscode("guest");
-        */
+                */
     }
 }
